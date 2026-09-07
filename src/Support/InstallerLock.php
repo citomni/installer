d81445bd619760de0a937fa2c5f0cf7933e16a95
Lars Mortensen <lars@grove-mortensen.dk>
2026-09-07 19:42:11 +0200

    Streamlined license headers

--- src/Support/InstallerLock.php
+++ src/Support/InstallerLock.php
@@ -2,8 +2,15 @@
 declare(strict_types=1);
 /*
  * This file is part of the CitOmni framework.
- * Copyright (c) 2012-present CitOmni.
+ * Low overhead, high performance, ready for anything.
+ *
+ * For more information, visit https://github.com/citomni
+ *
+ * Copyright (c) 2012-present Lars Grove Mortensen
  * SPDX-License-Identifier: MIT
+ *
+ * For full copyright, trademark, and license information,
+ * please see the LICENSE file distributed with this source code.
  */
 
 namespace CitOmni\Installer\Support;
